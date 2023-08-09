9e473076fb13d60e3cd75da183f0c5985ea66427
ashik pa <ashikpa003@gmail.com>
2023-08-09 13:15:10 +0400

    adding event binding features to inputs

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,10 +7,29 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   title : string = "login form"
-  imgUrl : string = "https://o.remove.bg/downloads/31820fae-b975-4ca7-860e-009c998e02c2/free-user-group-icon-296-thumb-removebg-preview.png"
-  login(){
-    alert(`login clicked`)
-  }
+  imgUrl : string = "https://o.remove.bg/downloads/9572cbd9-fc7f-47af-a01d-5b757de75b8c/corporate-user-icon-removebg-preview.png"
+ 
+  userName : string=""
+  password : string=""
+  userNameChange(event:any){
+console.log(`username:`,event.target.value);
+this.userName = event.target.value
 
+  }
+  passwordChanger(event:any){
+    console.log(`password:`,event.target.value);
+    this.password = event.target.value
+    
+
+  }
+  login(){
+    if(this.userName && this.password){
+      alert(`username : ${this.userName},password : ${this.password}`)
+    }
+    else{
+      alert(`please enter the inputs`)
+    }
+
+  }
 
 }
